3c55fe4ed56f6589e3be558517b798919e3da0ba
Idriss Chaouch <62848804+CppProgrammer23@users.noreply.github.com>
2020-05-28 07:43:32 +0100

    Create Survival.build.cs

---
+++ Survival.build.cs
@@ -0,0 +1,25 @@
+// to create an advanced session with Steam
+
+using UnrealBuildTool;
+
+public class Survival : ModuleRules
+{
+	public Survival(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "OnlineSubsystemUtils", "Steamworks", "Networking" });
+
+		DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
